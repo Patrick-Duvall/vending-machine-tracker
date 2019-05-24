c287cc547784140b74d1e1a700223e287931e89c
Patrick Duvall <duvallp208@gmail.com>
2019-05-24 09:32:51 -0600

    Story 2 no 0 on average

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Machine, type: :model do
     it { should have_many :snacks}
 
     end
+
+    it ".average_price" do
+      @owner = Owner.create!(name: "Tom")
+      @machine = @owner.machines.create!(location: "Mall")
+      @candy = @machine.snacks.create!(name: "Twix", price: 1.25)
+      @chips = @machine.snacks.create!(name: "Lays", price: 1.75)
+      @chipers = @machine.snacks.create!(name: "Pringles", price: 1.5)
+      expect(@machine.average_snack_price).to eq(1.5)
+    end
   end
